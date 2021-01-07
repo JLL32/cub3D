7af14348c919756d4bcb397693557cccd2db6f9e
Abdeljalil El hachimi <ael-hach@e1r6p12.1337.ma>
2021-01-07 19:16:46 +0100

    adds essential data

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include "cub3D.h"
 
-int		main(int argc, char **argv)
+int		main(int argc, char *argv[argc])
 {
-	return 0;
+	double posX = 22, posY = 12; /*x and y start position*/
+	double dirX = -1, dirY = 0; /*initial direction vector*/
+	double planeX = 0, planeY = 0.66; /*the 2D raycaster version of camera plane*/
+
+	double time = 0; /*time of the current frame*/
+	double oldTime = 0; /*time of the previous frame*/
+
+	void *mlx;
+	void *mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, screenWidth, screenHeight, "raycaster");
+	mlx_loop(mlx);
+
+	return (0);
 }
