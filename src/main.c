48329fa719c9fb2f05a18de46400c7f182e17dbf
Abdeljalil El hachimi <ael-hach@e1r6p12.1337.ma>
2021-01-13 19:28:50 +0100

    adds textured walls

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "cub3D.h"
 #include "mlx/mlx.h"
+#include <math.h>
 
 typedef struct  s_vars {
 	void        *mlx;
@@ -9,6 +10,8 @@ typedef struct  s_vars {
 typedef struct  s_data {
 	void        *img;
 	int        *addr;
+	int			width;
+	int			height;
 	int         bits_per_pixel;
 	int         line_length;
 	int         endian;
@@ -53,7 +56,15 @@ t_data  img;
 
 t_vars vars;
 
+typedef struct s_texture {
+	void *add;
+	int width;
+	int height;
+} t_texture;
+
 double plane_x, plane_y; /*the 2D raycaster version of camera plane*/
+
+t_data textures[8];
 
 void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -153,6 +164,7 @@ int key_press(int keycode, t_player *player)
 	return 0;
 }
 
+
 int game_loop(t_player *player)
 {
 	for(int x = 0; x < screen_width; x++)
@@ -182,7 +194,7 @@ int game_loop(t_player *player)
 		int hit = 0; //was there a wall hit?
 		int side; //was a NS or a EW wall hit?
 
-		//calculate step and initial sideDist
+		//calculate step and initial side_dist
 		if (ray_dir_x < 0)
 		{
 			step_x = -1;
@@ -238,24 +250,87 @@ int game_loop(t_player *player)
 		if (draw_end >= screen_height) draw_end = screen_height - 1;
 		
 		// choose wall color
-		t_hex_color color;
-		switch(worldMap[map_x][map_y])
-		{
-		case 1:  color = red;  break;
-		case 2:  color = green;  break;
-		case 3:  color = blue;   break;
-		case 4:  color = white;  break;
-		default: color = yellow; break;
-		}
+		/* t_hex_color color; */
+		/* switch(worldMap[map_x][map_y]) */
+		/* { */
+		/* case 1:  color = red;  break; */
+		/* case 2:  color = green;  break; */
+		/* case 3:  color = blue;   break; */
+		/* case 4:  color = white;  break; */
+		/* default: color = yellow; break; */
+		/* } */
 		
-		// gives x and y different brightness
-		if (side == 1) color = color / 2;
+		/* // gives x and y different brightness */
+		/* if (side == 1) color = color / 2; */
 
-		vertical_line(x, draw_start, draw_end, color, vars, &img);
+		// texturing calculations
+		int tex_num = worldMap[map_x][map_y] - 1;
+
+		// calculate the value of wall_x
+		double wall_x; // where exactly the wall was hit
+		if (side == 0) wall_x = player->pos_y + perp_wall_dist * ray_dir_y;
+		else			wall_x = player->pos_x + perp_wall_dist * ray_dir_x;
+		wall_x -= floor(wall_x);
+		
+		// x coordinate on the texture
+		int tex_x = (int)(wall_x * (double)(tex_width));
+		if(side == 0 && ray_dir_x > 0) tex_x = tex_width - tex_x - 1;
+		if(side == 1 && ray_dir_y < 0) tex_x = tex_width - tex_x - 1;
+
+		// How much to increase the texture coordinate per screen pixel
+		double step = 1.0 * tex_height / line_height;
+		// Starting texture coordinat
+		double tex_pos = (draw_start - screen_height / 2.0 + line_height / 2.0) * step;
+
+		// draw ceiling
+		/* for (int y = 0; y <= draw_start; y++) */ 
+		/* { */
+		/* 	my_mlx_pixel_put(&img, x, y, light_blue); */
+		/* } */
+		/* for (int y = draw_start; y <= draw_end; y++) */
+		/* { */
+		/* 	// cast the texture coordinat to integer, and mask with (tex_height - 1) in case of overflow */
+		/* 	int text_y = (int)tex_pos & (tex_height - 1); */
+		/* 	tex_pos += step; */
+		/* 	int color = textures[tex_num].addr[tex_height * text_y + tex_x]; */
+		/* 	// make color darker for y-sides: R, G, B byte each divided through two with a shift and an and */
+		/* 	if (side == 1) color = (color >> 1) & 8355711; */
+		/* 	my_mlx_pixel_put(&img, x, y, color); */
+		/* } */
+		/* for (int y = draw_end; y <= screen_height; y++) */
+		/* { */
+		/* 	my_mlx_pixel_put(&img, x, y, gray); */
+		/* } */
+
+		int y = 0;
+		while (y < draw_start)
+		{
+			my_mlx_pixel_put(&img, x, y, light_blue);
+			y++;
+		}
+		while (y < draw_end)
+		{
+			// cast the texture coordinat to integer, and mask with (tex_height - 1) in case of overflow
+			int text_y = (int)tex_pos & (tex_height - 1);
+			tex_pos += step;
+			int color = textures[tex_num].addr[tex_height * text_y + tex_x];
+			// make color darker for y-sides: R, G, B byte each divided through two with a shift and an and
+			if (side == 1) color = (color >> 1) & 8355711;
+			my_mlx_pixel_put(&img, x, y, color);
+			y++;
+		}
+		while (y < screen_height)
+		{
+			my_mlx_pixel_put(&img, x, y, gray);
+			y++;
+		}
+
+		/* vertical_line(x, draw_start, draw_end, color, vars, &img); */
 	}
 	//Updates the screen.  Has to be called to view new pixels, but use only after
 	//drawing the whole screen because it's slow.
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	for(int y = 0; y < screen_height; y++) for(int x = 0; x < screen_width; x++) my_mlx_pixel_put(&img, x, y, 0);
 	// TODO: clear the screen with a recycled image using mlx_put_image_to_window
 	/* clear(); */
 	/* mlx_destroy_image(vars.mlx, img.img); */
@@ -271,15 +346,39 @@ int game_loop(t_player *player)
 
 int		main(int argc, char *argv[argc])
 {
-	t_player player = {.pos_x = 22, .pos_y = 12, .dir_x = -1, .dir_y = 0, .move_speed = 1};
+	t_player player = {.pos_x = 22, .pos_y = 11.5, .dir_x = -1, .dir_y = 0};
 	plane_x = 0;
 	plane_y = .66;
+	
 
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, screen_width, screen_height, "Cub3D");
 	img.img = mlx_new_image(vars.mlx, screen_width, screen_height);
 	img.addr = (int *)mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
+	textures[0].img = mlx_xpm_file_to_image(vars.mlx, "../assets/eagle.xpm", &textures[0].width, &textures[0].height);
+	textures[0].addr = (int *)mlx_get_data_addr(textures[0].img, &textures[0].bits_per_pixel, &textures[0].line_length, &textures[0].endian);
+	
+	textures[1].img = mlx_xpm_file_to_image(vars.mlx, "../assets/redbrick.xpm", &textures[1].width, &textures[1].height);
+	textures[1].addr = (int *)mlx_get_data_addr(textures[1].img, &textures[1].bits_per_pixel, &textures[1].line_length, &textures[1].endian);
+
+	textures[2].img = mlx_xpm_file_to_image(vars.mlx, "../assets/purplestone.xpm", &textures[2].width, &textures[2].height);
+	textures[2].addr = (int *)mlx_get_data_addr(textures[2].img, &textures[2].bits_per_pixel, &textures[2].line_length, &textures[2].endian);
+
+	textures[3].img = mlx_xpm_file_to_image(vars.mlx, "../assets/greystone.xpm", &textures[3].width, &textures[3].height);
+	textures[3].addr = (int *)mlx_get_data_addr(textures[3].img, &textures[3].bits_per_pixel, &textures[3].line_length, &textures[3].endian);
+
+	textures[4].img = mlx_xpm_file_to_image(vars.mlx, "../assets/bluestone.xpm", &textures[4].width, &textures[4].height);
+	textures[4].addr = (int *)mlx_get_data_addr(textures[4].img, &textures[4].bits_per_pixel, &textures[4].line_length, &textures[4].endian);
+
+	textures[5].img = mlx_xpm_file_to_image(vars.mlx, "../assets/mossy.xpm", &textures[5].width, &textures[5].height);
+	textures[5].addr = (int *)mlx_get_data_addr(textures[5].img, &textures[5].bits_per_pixel, &textures[5].line_length, &textures[5].endian);
+
+	textures[6].img = mlx_xpm_file_to_image(vars.mlx, "../assets/wood.xpm", &textures[6].width, &textures[6].height);
+	textures[6].addr = (int *)mlx_get_data_addr(textures[6].img, &textures[6].bits_per_pixel, &textures[6].line_length, &textures[6].endian);
+
+	textures[7].img = mlx_xpm_file_to_image(vars.mlx, "../assets/colorstone.xpm", &textures[7].width, &textures[7].height);
+	textures[7].addr = (int *)mlx_get_data_addr(textures[7].img, &textures[7].bits_per_pixel, &textures[7].line_length, &textures[7].endian);
 	mlx_loop_hook(vars.mlx, game_loop, &player);
 
 	/* mlx_key_hook(vars.win, key_press, &player); */
