a92c09b11e7101daf266354fc9c9ced95e3a50e0
Abdeljalil El hachimi <ael-hach@e1r8p10.1337.ma>
2021-02-08 16:07:37 +0100

    refactors sort_sprites.c by replacing for loops with while loops and adding some useful comments

--- src/sort_sprites.c
+++ src/sort_sprites.c
@@ -2,7 +2,8 @@
 
 static void swap(t_pair *xp, t_pair *yp)
 {
-	t_pair temp = *xp;
+	t_pair temp;
+	temp = *xp;
 	*xp = *yp;
 	*yp = temp;
 }
@@ -10,26 +11,45 @@ static void swap(t_pair *xp, t_pair *yp)
 static void pair_sort(t_pair arr[], int n)
 {
 	int i, j;
-	for (i = 0; i < n - 1; i++)
-		for (j = 0; j < n - i - 1; j++)
+
+	i = 0;
+	while (i < n - 1)
+	{
+		j = 0;
+		while (j < n - i - 1)
+		{
 			if (arr[j].first > arr[j + 1].first)
 				swap(&arr[j], &arr[j + 1]);
+			j++;
+		}
+		i++;
+	}
 }
+
+/*
+** initializes an array of two pairs with distance as the first pair then order
+** as the second one the sorts that collection of pairs in reverse order so we
+** can draw sprites from farthest to nearest
+*/
 
 void sort_sprites(int *order, double *dist, int amount)
 {
-	/* std::vector<std::pair<double, int>> sprites(amount); */
 	t_pair sprites[amount];
-	for (int i = 0; i < amount; i++)
+	int i;
+
+	i = 0;
+	while (i < amount)
 	{
 		sprites[i].first = dist[i];
 		sprites[i].second = order[i];
+		i++;
 	}
 	pair_sort(sprites, amount);
-	// restore in reverse order to go from farthest to nearest
-	for (int i = 0; i < amount; i++)
+	i = 0;
+	while (i < amount)
 	{
 		dist[i] = sprites[amount - i - 1].first;
 		order[i] = sprites[amount - i - 1].second;
+		i++;
 	}
 }
