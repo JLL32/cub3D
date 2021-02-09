3145ddcd87d047d8549db9a1e4ac2bf4d5e670b2
Abdeljalil El hachimi <ael-hach@e1r6p12.1337.ma>
2021-02-09 11:59:19 +0100

    refactors some comments in sprite.c

--- src/sprite.c
+++ src/sprite.c
@@ -1,30 +1,32 @@
 #include "../cub3D.h"
 
+/*
+** translate sprite position to relative to camera
+** transform sprite with the inverse camera matrix
+**  [ planeX   dirX ] -1                                       [ dirY      -dirX ]
+**  [               ]       =  1/(planeX*dirY-dirX*planeY) *   [                 ]
+**  [ planeY   dirY ]                                          [ -planeY  planeX ]
+*/
+
 static t_coordinate get_transform(t_player player, t_coordinate sprites[], int sprite_order[], int i)
 {
-
 	double sprite_x;
 	double sprite_y;
 	double inv_det;
 	t_coordinate transform;
 
-	// translate sprite position to relative to camera
 	sprite_x = sprites[sprite_order[i]].x - player.pos_x;
 	sprite_y = sprites[sprite_order[i]].y - player.pos_y;
-
-	//transform sprite with the inverse camera matrix
-	// [ planeX   dirX ] -1                                       [ dirY      -dirX ]
-	// [               ]       =  1/(planeX*dirY-dirX*planeY) *   [                 ]
-	// [ planeY   dirY ]                                          [ -planeY  planeX ]
-	inv_det = 1.0 / (player.plane_x * player.dir_y - player.dir_x * player.plane_y); // required for correct matrix multiplication
-
+	inv_det = 1.0 / (player.plane_x * player.dir_y - player.dir_x * player.plane_y);
 	transform.x = inv_det * (player.dir_y * sprite_x - player.dir_x * sprite_y);
 	transform.y = inv_det * (-player.plane_y * sprite_x + player.plane_x * sprite_y);
-
 	return (transform);
 }
 
-// calculates the lowest pixels to fill of a sprite's stripe
+/*
+** calculates the lowest pixels to fill of a sprite's stripe
+*/
+
 static t_square get_sprite_draw_start(t_sprite sprite, t_resolution res)
 {
 	t_square draw_start;
@@ -38,7 +40,10 @@ static t_square get_sprite_draw_start(t_sprite sprite, t_resolution res)
 	return (draw_start);
 }
 
-// calculates the highest pixels to fill of a sprite's stripe
+/*
+** calculates the highest pixels to fill of a sprite's stripe
+*/
+
 static t_square get_sprite_draw_end(t_sprite sprite, t_resolution res)
 {
 	t_square draw_end;
@@ -66,12 +71,15 @@ t_sprite get_sprite(t_game *game, int sprite_order[], int i)
 	return (sprite);
 }
 
-//loops through every vertical stripe of the sprite on screen and draws it
-//the conditions in the if are:
-//1) it's in front of camera plane so you don't see things behind you
-//2) it's on the screen (left)
-//3) it's on the screen (right)
-//4) ZBuffer, with perpendicular distance
+/*
+** loops through every vertical stripe of the sprite on screen and draws it
+** the conditions in the if are:
+** 1) it's in front of camera plane so you don't see things behind you
+** 2) it's on the screen (left)
+** 3) it's on the screen (right)
+** 4) ZBuffer, with perpendicular distance
+*/
+
 void draw_sprite(t_game *game, t_sprite sprite, double z_buffer[])
 {
 	t_square tex_px;
@@ -87,13 +95,13 @@ void draw_sprite(t_game *game, t_sprite sprite, double z_buffer[])
 		if (sprite.trans.y > 0 && stripe > 0 && stripe < game->res.width && sprite.trans.y < z_buffer[stripe])
 		{
 			y = sprite.draw_start.y;
-			while (y < sprite.draw_end.y) //for every pixel of the current stripe
+			while (y < sprite.draw_end.y)
 			{
-				d = (y)*256 - game->res.height * 128 + sprite.res.height * 128; //256 and 128 factors to avoid floats
+				d = (y)*256 - game->res.height * 128 + sprite.res.height * 128;
 				tex_px.y = ((d * TEX_HEIGHT) / sprite.res.height) / 256;
-				color = game->textures[SP_TEX].addr[TEX_WIDTH * tex_px.y + tex_px.x]; //get current color from the texture
+				color = game->textures[SP_TEX].addr[TEX_WIDTH * tex_px.y + tex_px.x];
 				if ((color & 0x00FFFFFF) != 0)
-					my_mlx_pixel_put(game, stripe, y, color); //paint pixel if it isn't black, black is the invisible color
+					my_mlx_pixel_put(game, stripe, y, color);
 				y++;
 			}
 		}
