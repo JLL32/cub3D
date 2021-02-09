1f26f955641c815229f703bfb2e063e911ceb291
Abdeljalil El hachimi <ael-hach@e1r6p12.1337.ma>
2021-02-09 11:52:40 +0100

    moves tokens checking in save_color.c into one function

--- parser/src/save_color.c
+++ parser/src/save_color.c
@@ -23,26 +23,30 @@ static void count_element(char **element)
 		ft_error(NULL, "Error\nInvalid number of color arguments\n");
 }
 
-static void count_tokens(char *element)
-{
-	int token_count;
-
-	token_count = 1;
-	while (*element)
-	{
-		if (*element == ',')
-			token_count++;
-		element++;
-	}
-	if (token_count > 3)
-		ft_error(NULL, "Error\nInvalid number of colors");
-}
-
 static int create_rgb(int r, int g, int b)
 {
 	if (r > 255 || g > 255 || b > 255)
 		ft_error(NULL, "Error\nRgb value must not be more than 255");
 	return (((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff));
+}
+
+static void check_every_token(char *element)
+{
+	int i;
+	int token_count;
+
+	i = 0;
+	token_count = 1;
+	while (element[i])
+	{
+		if (element[i] == ',')
+			token_count++;
+		else if (element[i] < '0' || element[i] > '9')
+			ft_error(NULL, "Error\nColor information are incorrect\n");
+		i++;
+	}
+	if (token_count > 3)
+		ft_error(NULL, "Error\nInvalid number of colors");
 }
 
 int save_color(char **element)
@@ -58,14 +62,7 @@ int save_color(char **element)
 	if (i != 2 || color_num > 1)
 		ft_error(NULL, "Error\nColor information are incorrect\n");
 	color_num++;
-	count_tokens(element[1]);
-	i = 0;
-	while (element[1][i])
-	{
-		if (element[1][i] != ',' && (element[1][i] < '0' || element[1][i] > '9'))
-			ft_error(NULL, "Error\nColor information are incorrect\n");
-		i++;
-	}
+	check_every_token(element[1]);
 	color_char = ft_split(element[1], ',');
 	count_element(color_char);
 	color = create_rgb(ft_atoi(color_char[0]), ft_atoi(color_char[1]),
